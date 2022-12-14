480a21b0c1036e3f03fadf2934d7ef03d671ebf0
Alex Rufino <frameworktecnologia26@localiza.com>
2022-12-14 16:04:07 -0300

    Teste negativo

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,12 @@
 describe('empty spec', () => {
-  it('passes', () => {
+  beforeEach(() => {
     cy.visit('https://alexronny91.github.io/testeCI/')
+  })
+  it('sucess', () => {
+    cy.get('h1').should('contain', 'Teste')
+    cy.get('h1').should('contain', 'Cypress')
+  })
+  it('fail', () => {
+    cy.get('h1').should('contain', 'CI')
   })
 })
